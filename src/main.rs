be5a87ec6c25be01173d257424496bf1dd0bd4c2
Sam <sam@teamzoon.com>
2021-02-25 18:56:29 +0800

    add substraction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::convert::TryInto;
 use std::io;
 use std::time::SystemTime;
@@ -26,13 +27,38 @@ fn calculate(max_count: u32, num_range: u32) {
     loop {
         let num1 = rand::thread_rng().gen_range(1..num_range);
         let num2 = rand::thread_rng().gen_range(1..num_range);
+        let operater = rand::thread_rng().gen_range(0..2);
 
-        println!("({}) {} + {} = __ ", count + 1, num1, num2);
+        let big: u32;
+        let small: u32;
 
-        if answer_check(num1, num2) {
+        match num1.cmp(&num2) {
+            Ordering::Less => {
+                big = num2;
+                small = num1;
+            }
+            Ordering::Greater => {
+                big = num1;
+                small = num2;
+            }
+            Ordering::Equal => {
+                big = num1;
+                small = num2;
+            }
+        }
+
+        if operater == 0 {
+            println!("({}) {} + {} = __ ", count + 1, big, small);
+        } else {
+            println!("({}) {} - {} = __ ", count + 1, big, small);
+        }
+
+        let answer = input_number(u32::MIN, u32::MAX);
+
+        if answer_check(answer, operater, big, small) {
             score += 1;
         } else {
-            failed.push([num1, num2]);
+            failed.push([big, small, operater]);
         }
 
         count += 1;
@@ -53,8 +79,12 @@ fn calculate(max_count: u32, num_range: u32) {
     }
     if score != max_count {
         println!("错题: {}", failed.len());
-        for [num1, num2] in &failed {
-            println!("{} + {} = __", num1, num2);
+        for [bg, sm, op] in &failed {
+            if op == &0 {
+                println!("{} + {} = __ ", bg, sm);
+            } else {
+                println!("{} - {} = __ ", bg, sm);
+            }
         }
 
         println!("是否订正? (确定请输入y)");
@@ -66,12 +96,18 @@ fn calculate(max_count: u32, num_range: u32) {
         if choose.trim() == "y" {
             while failed.len() > 0 {
                 if let Some(nums) = failed.pop() {
-                    println!("{} + {} = __", nums[0], nums[1]);
+                    if nums[2] == 0 {
+                        println!("{} + {} = __ ", nums[0], nums[1]);
+                    } else {
+                        println!("{} - {} = __ ", nums[0], nums[1]);
+                    }
 
-                    if answer_check(nums[0], nums[1]) {
+                    let answer = input_number(u32::MIN, u32::MAX);
+
+                    if answer_check(answer, nums[2], nums[0], nums[1]) {
                         println!("回答正确!");
                     } else {
-                        failed.push([nums[0], nums[1]]);
+                        failed.push([nums[0], nums[1], nums[2]]);
                         println!("回答错误!");
                     }
                 }
@@ -87,20 +123,14 @@ fn time_format(time: u32) -> Vec<u32> {
     [time / 60, time % 60].to_vec()
 }
 
-fn answer_check(num1: u32, num2: u32) -> bool {
-    let answer: u32;
-    loop {
-        let mut input = String::new();
-
-        io::stdin().read_line(&mut input).expect("发生了一些错误");
-
-        answer = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        break;
-    }
-    answer == num1 + num2
+fn answer_check(answer: u32, op: u32, bg: u32, sm: u32) -> bool {
+    let correct_answer: u32;
+    if op == 0 {
+        correct_answer = bg + sm;
+    } else {
+        correct_answer = bg - sm;
+    };
+    answer == correct_answer
 }
 
 fn input_number(low: u32, high: u32) -> u32 {
